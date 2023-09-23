22006414c06540179888a750d6c0d4b879b7f999
HadRad500 <hadignasr@gmail.com>
2023-09-22 17:42:56 -0700

    updated movies and added divs to movie-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,26 +6,32 @@ export const MainView = () => {
     const [movies, setMovies] = useState([
         {
             id: 1,
-            title: "Eloquent JavaScript",
+            title: "Get Shorty",
+            description: "The story follows a Miami mobster who goes to LA to collect some outstanding debts, and in the process becomes a movie producer.",
+            genre: "Comedy",
             image:
-                "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-            director: "Marijn Haverbeke"
+                "https://www.originalfilmart.com/cdn/shop/products/Get_Shorty_1995_original_film_art_56860cce-91f7-43d9-ac4b-e415e6f81c8b.jpg?v=1633380611",
+            director: "Barry SonnenFeld"
         },
 
         {
             id: 2,
-            title: "Mastering JavaScript Functional Programming",
+            title: "Dune",
+            description: "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people.",
+            genre: "Science Fiction",
             image:
-                "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-            director: "Federico Kereki"
+                "https://m.media-amazon.com/images/I/61QbqeCVm0L.jpg",
+            director: "Denis Villeneuve"
         },
 
         {
             id: 3,
-            title: "JavaScript: The Good Parts",
+            title: "Wall-E",
+            description: "WALL-E, short for Waste Allocation Load Lifter Earth-class, is the last robot left on Earth. He spends his days tidying up the planet, one piece of garbage at a time.",
+            genre: "Animated",
             image:
-                "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-            director: "Douglas Crockford"
+                "https://m.media-amazon.com/images/I/51RoZRgIHtL._AC_UF894,1000_QL80_.jpg",
+            director: "Andrew Stanton"
         },
 
     ]);
